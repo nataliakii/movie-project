db81184833ecfdf585d63c7ef5b5f7f6d40c658b
Natalia Kirejeva <nataliaki@icloud.com>
2022-11-11 15:26:12 +0200

    Ready to deploy

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const keys = require('./config/keys');
 
+
+
 // DB Setup
 mongoose.connect(keys.MONGODB_URI, {
   useNewUrlParser: true,
@@ -18,6 +20,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 router(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 // Server Setup
 const port = process.env.PORT || 5000;
